79c61f5daff42da6d05928c224479d0112f9018f
mocatfrio <hafarafirdausi@gmail.com>
2018-10-22 04:07:09 +0700

    done

--- Tugas-ETS/create_proxysql_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitorpassword';
-GRANT SELECT on sys.* to 'monitor'@'%';
-FLUSH PRIVILEGES;
-
-CREATE USER 'playgrounduser'@'%' IDENTIFIED BY 'playgroundpassword';
-GRANT ALL PRIVILEGES on playground.* to 'playgrounduser'@'%';
-FLUSH PRIVILEGES;
